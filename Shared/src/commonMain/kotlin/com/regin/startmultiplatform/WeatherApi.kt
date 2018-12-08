fbe7dae699342a82c4e2875a14c265144cb80c77
ReginFell <ReginFell@gmail.com>
2018-12-08 22:00:53 +0200

    Add Api integration

---
+++ Shared/src/commonMain/kotlin/com/regin/startmultiplatform/WeatherApi.kt
@@ -0,0 +1,34 @@
+package com.regin.startmultiplatform
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import io.ktor.client.request.url
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
+
+class WeatherApi(private val engine: HttpClientEngine) {
+
+    private val client by lazy {
+        HttpClient(engine) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(JSON.nonstrict)
+            }
+        }
+    }
+
+    suspend fun fetchWeather(): Weather {
+        return client.get {
+            url("$baseUrl/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")
+        }
+    }
+
+    companion object {
+        private const val baseUrl = "https://samples.openweathermap.org"
+    }
+
+    @Serializable
+    data class Weather(val base: String)
+}
--- app/src/main/java/com/regin/startmultiplatform/MainActivity.kt
+++ app/src/main/java/com/regin/startmultiplatform/MainActivity.kt
@@ -1,17 +1,42 @@
 package com.regin.startmultiplatform
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.regin.com.regin.startmultiplatform.R
-import kotlinx.android.synthetic.main.activity_main.*
+import io.ktor.client.engine.okhttp.OkHttpConfig
+import io.ktor.client.engine.okhttp.OkHttpEngine
+import kotlinx.coroutines.*
+import okhttp3.logging.HttpLoggingInterceptor
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        shared.text = provideSharedData()
-        platform.text = providePlatformData()
+        val weatherApi = WeatherApi(OkHttpEngine(OkHttpConfig().apply {
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        }))
+        launch(Dispatchers.Main) {
+            try {
+                val result = withContext(Dispatchers.IO) { weatherApi.fetchWeather() }
+                Toast.makeText(this@MainActivity, result.toString(), Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
